f80d7042de64755ca99f0167140ed96db279f4ce
takashi3526 <Yamamoto.Takashi@softcreate.co.jp>
2025-07-23 17:32:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/tokaido_53_stations.css'; // CSSファイルの読み込み
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
