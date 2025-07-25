d570a189222446508ae914cdca8bd87b8548bcbb
takashi3526 <Yamamoto.Takashi@softcreate.co.jp>
2025-07-25 13:25:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import AuthButtons from './components/AuthButtons';
+import ProgressBar from './components/ProgressBar';
+import FilterTabs from './components/FilterTabs';
+import StationList from './components/StationList';
 import './styles/tokaido_53_stations.css';
-
 function App() {
+  const [filter, setFilter] = useState('すべて');
   return (
     <div className="container">
-      <h1>東海道53次テスト</h1>
-      <p>これが表示されれば、App.jsは正常に動作しています。</p>
+      <Header />
+      <AuthButtons />
+      <ProgressBar visited={0} total={53} />
+      <FilterTabs onFilterChange={setFilter} />
+      <StationList filter={filter} />
     </div>
   );
 }
-
 export default App;
